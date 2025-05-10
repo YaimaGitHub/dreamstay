28097600d34df5a8c9d60b8ee908363e45bc6ef5
YaimaGitHub <tvalacarta33@gmail.com>
2025-05-09 20:32:23 -0400

    -
    -

--- src/components/ui/carousel.tsx
+++
@@ -1,9 +0,0 @@
-// This file now re-exports all carousel components from the carousel directory
-export {
-  type CarouselApi,
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
-} from "./carousel/index"
--- src/components/ui/carousel/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { CarouselApi } from "./CarouselContext"
-import { Carousel } from "./Carousel"
-import { CarouselContent } from "./CarouselContent"
-import { CarouselItem } from "./CarouselItem"
-import { CarouselPrevious } from "./CarouselPrevious"
-import { CarouselNext } from "./CarouselNext"
-
-export { type CarouselApi, Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext }
